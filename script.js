1306c65fc29ef838517038aa0b1a568c29536271
geertza <geertza@gmail.com>
2020-04-08 19:34:52 -0600

    brew api display

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // Search by zip code https://api.openbrewerydb.org/breweries?by_postal=44107
 
 let searchResults = [];
-
+    brewList =""
 
 
 
@@ -63,7 +63,7 @@ function byZip() {
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-
+        brewList = response;
         //variables to capture API response properties
         let name = response[2].name;
         console.log(name);
@@ -97,3 +97,54 @@ $("PLACEHOLDER FOR SEARCH BUTTON").on("click", function(event) {
     renderSearchResults();
     console.log(searchTerm);
 });
+
+  // function for brewery list
+  function addbrew(){
+    for (i=0; i< brewList.length; i++){
+        $(".brewbox").remove()
+      name = brewList[i].name
+      city = brewList[i].city
+      address = brewList[i].street 
+      type = brewList[i].brewery_type
+      phone = phone ='('+ brewList[i].phone.slice(0,3)+")"+brewList[i].phone.slice(3,6)+'-'+brewList[i].phone.slice(6,10)
+      website = brewList[i].website_url
+      newContainer = document.createElement('div')
+      newContainer.className = 'brewBox container grid'
+      newContainer.id = "brewBox"+i
+      
+      
+      //  images for different types
+      if (type === "micro"){
+        newContainer.style.backgroundImage =' url("./images/micro.png")';
+      }
+      if (type === "regional"){
+        newContainer.style.backgroundImage =' url("./images/regional.png")';
+      } if (type === "brewpub"){
+        newContainer.style.backgroundImage =' url("./images/brewpub.png")';
+      } if (type === "large"){
+        newContainer.style.backgroundImage =' url("./images/large.png")';
+      } if (type === "bar"){
+        newContainer.style.backgroundImage =' url("./images/bar.png")';
+      } if (type === "contract"){
+        newContainer.style.backgroundImage =' url("./images/contract.png")';
+      } if (type === "planning"){
+        newContainer.style.backgroundImage =' url("./images/planning.png")';
+      }
+      if (type === "proprieter"){
+        newContainer.style.backgroundImage =' url("./images/proprieter.png")';
+      }
+      
+      // Creats divs for brewery list
+      $("search-results").append(newContainer)
+      const varList=[name,city,address,phone]  
+      altVarList=["name",'city','address','phone']
+      for (x=0; x < varList.length; x++){
+        g = varList[x] , h = altVarList[x] 
+         newDiv = document.createElement('div')
+           newDiv.className = h
+            newDiv.append(g)  
+        newContainer.append(newDiv);}
+        const webButton =  document.createElement('a')
+        newContainer.append(webButton)
+        webButton.outerHTML = "<a class=website href="+website+">"+website+"</a>"
+        }}
